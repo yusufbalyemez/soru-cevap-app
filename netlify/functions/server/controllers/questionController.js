a8a8de9b87d4ab7f44d818eafcc7e4c78d7febc2
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-07-03 19:37:15 +0300

    netlify ok

--- netlify/functions/server/controllers/questionController.js
+++ netlify/functions/server/controllers/questionController.js
@@ -1,93 +1,125 @@
-const fs = require('fs');
-const path = require('path');
+const Test = require('../models/TestModel'); // Modelinizi doğru yola göre değiştirin
 
-const dataFilePath = path.join(__dirname, '../data/questions.json');
-
-// Helper function to read data from JSON file
-const readData = () => {
-  const data = fs.readFileSync(dataFilePath);
-  return JSON.parse(data);
+// Tüm testleri al
+exports.getAllTests = async (req, res) => {
+  try {
+    const tests = await Test.find();
+    res.json(tests);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching tests', error: err });
+  }
 };
 
-// Helper function to write data to JSON file
-const writeData = (data) => {
-  fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
+// Tek bir testi al
+exports.getTestById = async (req, res) => {
+  const { testId } = req.params;
+  try {
+    const test = await Test.findById(testId);
+    if (!test) {
+      return res.status(404).json({ message: 'Test not found' });
+    }
+    res.json(test);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching test', error: err });
+  }
 };
 
-exports.getAllTests = (req, res) => {
-  const data = readData();
-  res.json(data.tests);
-};
-
-exports.addTest = (req, res) => {
-  const data = readData();
+// Yeni test ekle
+exports.addTest = async (req, res) => {
   const { testName } = req.body;
-
-  if (data.tests.find(test => test.testName === testName)) {
-    return res.status(400).json({ message: 'Test already exists' });
+  try {
+    // Test isminin zaten var olup olmadığını kontrol et
+    const existingTest = await Test.findOne({ testName });
+    if (existingTest) {
+      return res.status(400).json({ message: 'Test already exists' });
+    }
+    const newTest = new Test({ testName, questions: [] });
+    await newTest.save();
+    res.status(201).json(newTest);
+  } catch (err) {
+    res.status(500).json({ message: 'Error adding test', error: err });
   }
-
-  const newTest = {
-    testName,
-    questions: []
-  };
-
-  data.tests.push(newTest);
-  writeData(data);
-  res.status(201).json(newTest);
 };
 
-exports.addQuestion = (req, res) => {
-  const data = readData();
-  const { testIndex } = req.params;
+// Teste soru ekle
+exports.addQuestion = async (req, res) => {
+  const { question, answer,testId } = req.body;
+
+  try {
+    const test = await Test.findById(testId);
+    if (!test) {
+      return res.status(404).json({ message: 'Test not found' });
+    }
+    test.questions.push({ question, answer });
+    await test.save();
+    res.status(201).json(test);
+  } catch (err) {
+    res.status(500).json({ message: 'Error adding question', error: err });
+  }
+};
+
+// Soruyu sil
+exports.deleteQuestion = async (req, res) => {
+  const { testId, questionId } = req.params;
+  try {
+    const test = await Test.findByIdAndUpdate(testId, {
+      $pull: { questions: { _id: questionId } }
+    }, { new: true });
+    if (!test) {
+      return res.status(404).json({ message: 'Test or Question not found' });
+    }
+    res.status(200).json(test);
+  } catch (err) {
+    res.status(500).json({ message: 'Error deleting question', error: err });
+  }
+};
+
+// Soruyu güncelle
+exports.updateQuestion = async (req, res) => {
+  const { testId, questionId } = req.params;
   const { question, answer } = req.body;
-
-  if (data.tests[testIndex]) {
-    data.tests[testIndex].questions.push({ question, answer });
-    writeData(data);
-    res.status(201).json(data.tests[testIndex]);
-  } else {
-    res.status(404).json({ message: 'Test not found' });
+  try {
+    const test = await Test.findOneAndUpdate({ "_id": testId, "questions._id": questionId }, {
+      "$set": {
+        "questions.$.question": question,
+        "questions.$.answer": answer
+      }
+    }, { new: true });
+    if (!test) {
+      return res.status(404).json({ message: 'Test or Question not found' });
+    }
+    res.status(200).json(test);
+  } catch (err) {
+    res.status(500).json({ message: 'Error updating question', error: err });
   }
 };
 
-exports.deleteQuestion = (req, res) => {
-  const data = readData();
-  const { testIndex, questionIndex } = req.params;
-
-  if (data.tests[testIndex] && data.tests[testIndex].questions[questionIndex]) {
-    data.tests[testIndex].questions.splice(questionIndex, 1);
-    writeData(data);
-    res.status(200).json(data.tests[testIndex]);
-  } else {
-    res.status(404).json({ message: 'Test or Question not found' });
-  }
-};
-
-exports.updateQuestion = (req, res) => {
-  const data = readData();
-  const { testIndex, questionIndex } = req.params;
-  const { question, answer } = req.body;
-
-  if (data.tests[testIndex] && data.tests[testIndex].questions[questionIndex]) {
-    data.tests[testIndex].questions[questionIndex] = { question, answer };
-    writeData(data);
-    res.status(200).json(data.tests[testIndex]);
-  } else {
-    res.status(404).json({ message: 'Test or Question not found' });
-  }
-};
-
-exports.updateTestName = (req, res) => {
-  const data = readData();
-  const { testIndex } = req.params;
+// Test adını güncelle
+exports.updateTestName = async (req, res) => {
+  const { testId } = req.params;
   const { testName } = req.body;
-
-  if (data.tests[testIndex]) {
-    data.tests[testIndex].testName = testName;
-    writeData(data);
-    res.status(200).json(data.tests[testIndex]);
-  } else {
-    res.status(404).json({ message: 'Test not found' });
+  try {
+    const test = await Test.findByIdAndUpdate(testId, { testName }, { new: true });
+    if (!test) {
+      return res.status(404).json({ message: 'Test not found' });
+    }
+    res.status(200).json(test);
+  } catch (err) {
+    res.status(500).json({ message: 'Error updating test name', error: err });
   }
 };
+
+// Test sil
+exports.deleteTest = async (req, res) => {
+  const { testId } = req.params;
+  try {
+    const test = await Test.findOneAndDelete({ _id: testId });
+    if (!test) {
+      return res.status(404).json({ message: 'Test not found' });
+    }
+    res.status(200).json({ message: 'Test deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error deleting test', error: err });
+  }
+};
+
